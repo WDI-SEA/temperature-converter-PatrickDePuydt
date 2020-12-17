99a12f16c8363d739353ed018a3836153412937d
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-16 18:30:15 -0700

    121620-PatrickDePuydt: Simplify javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const submitButton = document.querySelector("input[type=submit]");
-    const feedback = document.querySelector("[data-target='feedback']");
+    const submitButton = document.querySelector("input[type=submit]"); // Submit button
+    const feedback = document.querySelector("[data-target='feedback']"); // Feedback Section
     
-    // 0°F − 32) × 5/9 = -17.78°C
-
-    // const convertToCelcius = (temperature) =>  (temperature - 32) * 5/9;
-    const convertToFahrenheit = (temperature) => { 
-         return (temperature * 5/9) + 32;
-    };
+    const convertToCelcius = (temperature) =>  (temperature * 5/9) + 32; // (0°C × 9/5) + 32 = 32°F        
+    const convertToFahrenheit = (temperature) => (temperature - 32) * 5/9; // (32°F − 32) × 5/9 = 0°C
    
     
     submitButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        const input = document.querySelector("[data-selector='input']").value;
+        event.preventDefault(); // Stop the form from submitting
         
-        // Validate numbers
-        // Run Conversion
-        convertToFahrenheit(input);
+        const input = Number(document.querySelector("[data-selector='input']").value); // Get the value of the input, make sure it's a number
+        const isCelcius = document.querySelector("[data-selector='radio_celcius']").checked; // Check to see if the celcius radio button is checked
 
-        // Update dom
-        feedback.innerHTML = convertToFahrenheit(input);
+        if (isCelcius) {
+            feedback.innerHTML = convertToCelcius(input);
+        } else {
+            feedback.innerHTML = convertToFahrenheit(input);
+        }
+
     });
 });
 
