85202cb7627c005f98d3e1b875e01e69047dc6d4
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-16 17:34:31 -0700

    121620-PatrickDePuydt: Add radio buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const submitButton = document.querySelector("input[type=submit]");
-    const feedback = document.querySelector("[data-target='feedback']");
-
+    
     submitButton.addEventListener("click", (event) => {
         event.preventDefault();
-
         const inputFieldValue = document.querySelector("[data-selector='input']").value;
+        const feedback = document.querySelector("[data-target='feedback']");
 
         feedback.innerHTML = inputFieldValue;
     });
