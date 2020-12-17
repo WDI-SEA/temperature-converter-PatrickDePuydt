02e3ee0d96b3716c3f0cf78127f609062f2f0c1b
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-16 17:58:55 -0700

    121620-PatrickDePuydt: Add calculation functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const submitButton = document.querySelector("input[type=submit]");
+    const feedback = document.querySelector("[data-target='feedback']");
+    
+    // 0°F − 32) × 5/9 = -17.78°C
+
+    const convertToCelcius = (temperature) => {
+        return (temperature - 32) * 5/9
+    };
+    const convertToFahrenheit = (temperature) => {
+        return (temperature * 5/9) + 32
+    };
+   
     
     submitButton.addEventListener("click", (event) => {
         event.preventDefault();
-        const inputFieldValue = document.querySelector("[data-selector='input']").value;
-        const feedback = document.querySelector("[data-target='feedback']");
-
-        feedback.innerHTML = inputFieldValue;
+        // Validate numbers
+        // Run Conversion
+        convertToCelcius(80);
+        convertToFahrenheit(0);
+        // Update dom
     });
 });
+
